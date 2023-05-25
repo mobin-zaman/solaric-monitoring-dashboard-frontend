cb6e4d599b9b2e33503e88a43bafbeab2e477d2d
rubiyet <rubiyetfardous@gmail.com>
2023-05-25 23:19:46 +0600

    404 page added

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,11 +7,11 @@ export default function Custom404() {
       <h2 className="text-2xl font-semibold text-gray-600">
         Sorry, page not found!
       </h2>
-      <p className="text-gray-500">
+      <p className="text-gray-500 py-1">
         The page you are looking for does not exist. It might have been moved or
         deleted.
       </p>
-      <Link href="/" className="px-4 py-2 mt-4 text-white bg-gray-700 rounded hover:bg-gray-600">
+      <Link href="/" className="px-4 py-2 mt-3 text-white bg-gray-700 rounded hover:bg-gray-600">
           Back to Home
       </Link>
     </div>
