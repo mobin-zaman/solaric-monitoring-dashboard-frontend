ee0e1496ec8cd61043c03010f161b87ebe9890cd
rubiyet <rubiyetfardous@gmail.com>
2023-09-12 03:00:58 +0600

    footer added

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -13,6 +13,7 @@ export default function Dashboard() {
         <Header />
         <div className="p-2 h-full bg-gray-200 overflow-y-auto" style={{height: "calc(100vh - 4rem)"}}>
           <Dashboard1 />
+          <span className="font-bold bg-green-700 text-white p-2 rounded-md ml-5 px-10 py-2.5">Powered by Solaric</span>
         </div>
       </div>
       </div>
