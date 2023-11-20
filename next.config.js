db902095c9e6ddfc7e59e4007e14f6be34a335fd
Sadat Jubayer <sadatjubayer@gmail.com>
2023-11-20 16:13:47 +0600

    Fix image issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["img1.igen-tech.com", "i.ibb.co"],
+    domains: ["img1.igen-tech.com", "i.ibb.co", "img1.solarmanpv.com"],
   },
 };
 
